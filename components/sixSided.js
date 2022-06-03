ba526422767ef6001d90f117576e4567529d0572
Michael <michaell10919@gmail.com>
2022-06-03 12:31:31 -0700

    Added Second Dice

--- components/sixSided.js
+++ components/sixSided.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Image, Button } from 'react-native';
 
 import Dice1 from './images/dice1.png';
@@ -8,79 +8,85 @@ import Dice4 from './images/dice4.png';
 import Dice5 from './images/dice5.png';
 import Dice6 from './images/dice6.png';
 
-class SixSided extends Component {
-    constructor(props) {
-        super(props);
+const SixSided = () => {
+    const [uri, setUri] = useState(Dice1);
+    const [uri2, setUri2] = useState(Dice1);
 
-        this.state = {
-            uri: Dice1
-        };
-    }
-
-    rollDice() {
+    rollDice = () => {
         let roll = Math.floor(Math.random() * 6) + 1;
-
-        console.log(roll);
 
         switch (roll) {
             case 1:
-                this.state.uri = Dice1;
+                setUri(Dice1);
                 break;
             case 2:
-                this.state.uri = Dice2;
+                setUri(Dice2);
                 break;
             case 3:
-                this.state.uri = Dice3;
+                setUri(Dice3);
                 break;
             case 4:
-                this.state.uri = Dice4;
+                setUri(Dice4);
                 break;
             case 5:
-                this.state.uri = Dice5;
+                setUri(Dice5);
                 break;
             case 6:
-                this.state.uri = Dice6;
+                setUri(Dice6);
                 break;
             default:
-                this.state.uri = Dice1;
+                setUri(Dice1);
         }
-    }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Image style={styles.image} source={uri} />
-                <Button
-                    title="Roll"
-                    onPress={() => this.rollDice()}
-                    style={styles.text}
-                ></Button>
-            </View>
-        );
-    }
-}
+        let roll2 = Math.floor(Math.random() * 6) + 1;
+
+        switch (roll2) {
+            case 1:
+                setUri2(Dice1);
+                break;
+            case 2:
+                setUri2(Dice2);
+                break;
+            case 3:
+                setUri2(Dice3);
+                break;
+            case 4:
+                setUri2(Dice4);
+                break;
+            case 5:
+                setUri2(Dice5);
+                break;
+            case 6:
+                setUri2(Dice6);
+                break;
+            default:
+                setUri2(Dice1);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Image style={styles.image} source={uri} />
+            <Image style={styles.image} source={uri2} />
+            <Button
+                title="Roll"
+                onPress={() => rollDice()}
+                accessibilityLabel="Roll a six sided die"
+            ></Button>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'red',
+        backgroundColor: '#800000',
         alignItems: 'center',
         justifyContent: 'center'
     },
     image: {
         width: 200,
         height: 200
-    },
-    text: {
-        fontSize: 26,
-        color: '#35BDD0',
-        marginTop: 30,
-        fontStyle: 'italic',
-        paddingHorizontal: 10,
-        borderColor: '#30475E',
-        borderRadius: 5,
-        borderWidth: 3,
-        fontWeight: 'bold'
     }
 });
 
